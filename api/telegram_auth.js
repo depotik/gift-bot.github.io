724c569c82b24d8ebaead0b4e5f4407bbd73f897
depot1k <sdn6509@gmail.com>
2025-08-12 17:48:18 +0300

    Update telegram_auth.js

--- api/telegram_auth.js
+++ api/telegram_auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const crypto = require('crypto');
+const { createHmac } = require('crypto');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { createClient } = require('redis');
